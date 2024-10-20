51334e27695debd5903674d18fb97f2fcaa088d9
Diksha555-creator <dikshasagar53@gmail.com>
2024-10-20 16:43:30 +0000

    var and const done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=14433
+let accountEmail="diksha@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2//not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="212122"
+accountCity="Bengluru"
+let accountstate;
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of isssue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
+
+
+
