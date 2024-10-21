4e793cce41693dd0d2632643fb2daefcac2192c2
Diksha555-creator <dikshasagar53@gmail.com>
2024-10-21 06:43:04 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  // we are using node js not browser
+
+console.log(3
+    +
+    3)  //code readibility should be high
+
+    console.log("Diksha")
