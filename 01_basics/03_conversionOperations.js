d9b4aecf99143ca817bcedde83eb26a493dc94c9
Diksha555-creator <dikshasagar53@gmail.com>
2024-10-22 17:39:31 +0000

    operators and more

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,48 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 
 let somenumber=33;
 let stringnumber= String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+//***********Operations******************* 
+
+let value=3
+let negvalue= -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1="Hello";
+let str2=" Diksha";
+let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);  //if string is definedn intitially then everything treated ae string
+// console.log(1+2+"2");   //vice versa
+
+// console.log(true);
+// console.log(+true);  // these should'nt be done
+// console.log(+"");
+
+
+//not preffered code  ,there ar ebetter ways t make the code look more readable and efficient
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
+
+
+
