036253f240fab339498866621763407cbc878430
Diksha555-creator <dikshasagar53@gmail.com>
2024-10-27 11:49:39 +0000

    chane in location of file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+//Primitive 
+
+//7 types: String, Number , Boolean, null, undefined ,Symbol, BigInt
+
+const score=100
+const scoreVAlue=100.5
+
+const ISloggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid=Symbol('123')
+ console.log(id===anotherid);
+// const bigNumber=3434546464598786n
+
+//reference (non- primituve)
+
+//array, objects, functions
+
+
+const heroes =['shaktiman', 'naagraj', 'doga']
+
+let myObj={
+    name:"Diksha",
+    age:22,
+}
+
+
+const myfundtion= function(){
+    console.log("Hello World");
+    
+}
+
+//typeof
+
+console.log(typeof outsideTemp);
+console.log(typeof heroes);
+console.log(typeof id);
+
+
+
