f69a548b1d1ea36063cc620bd435414ab928ce61
Diksha555-creator <dikshasagar53@gmail.com>
2024-11-02 12:51:04 +0000

    array1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,48 @@
 //arrays
 
 
-const myArr=[1,2,3,4,5,'true',"Diksha"]
-console.log(myArr[0]);
+// const myArr=[1,2,3,4,5,'true',"Diksha"]
+const myArr=[0,1,2,3,4,5]
+const myHeroes=["shaktiman","batman","spiderman"]
+
+const myarr2=new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//Array methods
+
+myArr.push(6);
+myArr.push(7);
+
+myArr.pop();
+
+// console.log(myArr);
+
+// myArr.unshift(9);
+// console.log(myArr);
+//  myArr.shift(9);
+//  console.log(myArr);
+
+//  console.log(myArr.includes(9));
+//  console.log(myArr.indexOf(2));
+ 
+ 
+const newArr= myArr.join();      //join binds the array and also coveerts it into string
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+// Slice , Splice
+
+
+console.log("A",myArr);
+
+const myn1= myArr.slice(1,3);
+console.log(myn1);
+
+console.log("B",myArr);
+
+const myn2= myArr.splice(1,3);     // the original array also gets changed in splice
+console.log("c",myArr);
+console.log(myn2);
