1d29d4748dff59af8985a11b55c446ebf6ab0a2f
Diksha555-creator <dikshasagar53@gmail.com>
2025-01-25 14:42:20 +0000

    change1

--- practice/01.js
+++ practice/01.js
@@ -1,6 +1,20 @@
-let score="30abc"
-console.log(typeof(score));
+// let score="30abc"
+// console.log(typeof(score));
 
-let valueinnumber=Number(score)
-console.log(typeof valueinnumber);
-console.log( valueinnumber);
+// let valueinnumber=Number(score)
+// console.log(typeof valueinnumber);
+// console.log( valueinnumber);
+
+let isloggedin= 1
+console.log(typeof(isloggedin));
+
+let booleanisloggedin= Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+let somenumber=33
+console.log(typeof somenumber);
+
+let stringloggedin= String(somenumber);
+console.log(stringloggedin);
+console.log(typeof stringloggedin);
+
