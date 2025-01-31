ce3c6e54d544e6d1d4cdf2a4f53c9c1649a8b7c0
Diksha555-creator <dikshasagar53@gmail.com>
2025-01-31 09:42:45 +0000

    conversions

---
+++ 01_basics/03.1_conversionOperations.js
@@ -0,0 +1,23 @@
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>1);
+// console.log(2<1);
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);        //these conversions cause problems cause null can convert into nan or 0
+console.log(null==0);     
+console.log(null>=0);
+/*the reason for them to give different output is that that the comparison operator (>=,=<,<,>)
+and the equality check (==) work differntly in js */
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+// strict check (===), not only checks the valuae but also datatype
+console.log("2"===2);
